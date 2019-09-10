a09b5028d81292c2921e00883ceb0974f1f9373b
Ani Sinanaj <ani.sinanaj@caffeina.com>
2019-09-10 22:08:17 +0200

    [+] Refactor entrypoint to use SELENIUM endpoint from ENV

--- docker-compose/build/entrypoint.sh
+++ docker-compose/build/entrypoint.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 set -euo pipefail
 
-url="$1"
-shift
-
-until wget -O- "$url"; do
-  >&2 echo "Selenium is unavailable - sleeping"
+until wget -O- "$SELENIUM"; do
+  echo "Selenium is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Selenium is up - executing command"
+echo "Selenium is up - executing command"
+
 exec "$@"
